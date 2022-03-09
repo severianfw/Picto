085ac878129e1654e27f8ac1c361f0da8524829c
Severian Fathich <severianfathich@gmail.com>
2022-03-09 12:24:20 +0700

    resolve PR review detail page

--- app/src/main/java/com/severianfw/picto/view/detail/PhotoDetailActivity.kt
+++ app/src/main/java/com/severianfw/picto/view/detail/PhotoDetailActivity.kt
@@ -5,15 +5,12 @@ import android.os.Bundle
 import com.bumptech.glide.Glide
 import com.severianfw.picto.databinding.ActivityDetailBinding
 import com.severianfw.picto.domain.model.PhotoItemModel
+import com.severianfw.picto.utils.Constant
 
-class DetailActivity : AppCompatActivity() {
+class PhotoDetailActivity : AppCompatActivity() {
 
     private var _binding: ActivityDetailBinding? = null
     private val binding get() = _binding!!
-
-    companion object {
-        const val TAG = "detail_activity"
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,18 +25,18 @@ class DetailActivity : AppCompatActivity() {
     }
 
     private fun setPhotoData() {
-        val image = intent.getParcelableExtra<PhotoItemModel>(TAG)
-        if (image != null) {
-            binding.apply {
-                Glide.with(this@DetailActivity)
-                    .load(image.mainImageUri)
+        val photoItem = intent.getParcelableExtra<PhotoItemModel>(Constant.PHOTO_ITEM)
+        if (photoItem != null) {
+            with(binding) {
+                Glide.with(this@PhotoDetailActivity)
+                    .load(photoItem.mainImageUri)
                     .into(ivPhoto)
-                if (image.description.isNotBlank()) {
-                    tvDesc.text = image.description
+                if (photoItem.description.isNotBlank()) {
+                    tvDesc.text = photoItem.description
                 }
-                tvUsername.text = image.authorName
-                Glide.with(this@DetailActivity)
-                    .load(image.profilePictureUri)
+                tvUsername.text = photoItem.authorName
+                Glide.with(this@PhotoDetailActivity)
+                    .load(photoItem.profilePictureUri)
                     .circleCrop()
                     .into(ivUserProfilePicture)
             }
