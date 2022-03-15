32983cd82a5972eb774e5e2bc9f5cafffbec46a9
Severian Fathich <severianfathich@gmail.com>
2022-03-15 16:58:41 +0700

    fix dark mode icon & background color in detail page

--- app/src/main/java/com/severianfw/picto/view/detail/PhotoDetailActivity.kt
+++ app/src/main/java/com/severianfw/picto/view/detail/PhotoDetailActivity.kt
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bumptech.glide.Glide
 import com.severianfw.picto.PictoApplication
+import com.severianfw.picto.R
 import com.severianfw.picto.databinding.ActivityDetailBinding
 import com.severianfw.picto.domain.model.PhotoItemModel
 import com.severianfw.picto.viewmodel.PhotoDetailViewModel
 import javax.inject.Inject
 import com.severianfw.picto.utils.Constant
+import com.severianfw.picto.utils.DarkModeUtil
 
 class PhotoDetailActivity : AppCompatActivity() {
 
@@ -25,11 +27,22 @@ class PhotoDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         _binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupIconColor()
 
         val photoItem = intent.getParcelableExtra<PhotoItemModel>(Constant.PHOTO_ITEM)
         setPhotoData(photoItem)
         setBackButtonListener()
         setDownloadButtonListener(photoItem)
+    }
+
+    private fun setupIconColor() {
+        if (DarkModeUtil.isDarkModeActive(resources)) {
+            Glide.with(this).load(R.drawable.ic_back_dark).into(binding.btnBack)
+            Glide.with(this).load(R.drawable.ic_download_dark).into(binding.btnDownload)
+        } else {
+            Glide.with(this).load(R.drawable.ic_back_light).into(binding.btnBack)
+            Glide.with(this).load(R.drawable.ic_download_light).into(binding.btnDownload)
+        }
     }
 
     private fun setBackButtonListener() {
