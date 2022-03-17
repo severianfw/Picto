c7b5883ea8df69a3935a94e529a66973694ff2d4
Severian Fathich <severianfathich@gmail.com>
2022-03-17 11:55:48 +0700

    add notification download completed

--- app/src/main/java/com/severianfw/picto/downloadmanager/DownloadManagerHelperImpl.kt
+++ app/src/main/java/com/severianfw/picto/downloadmanager/DownloadManagerHelperImpl.kt
@@ -1,19 +1,33 @@
 package com.severianfw.picto.downloadmanager
 
 import android.app.DownloadManager
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.net.Uri
+import android.widget.Toast
 import com.severianfw.picto.R
 import javax.inject.Inject
 
-class DownloadManagerHelperImpl @Inject constructor(private val context: Context) : DownloadManagerHelper {
+class DownloadManagerHelperImpl @Inject constructor(private val context: Context) :
+    DownloadManagerHelper {
 
     private val downloadManager: DownloadManager by lazy {
         context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
     }
 
-    override fun downloadPhoto(photoUrl: String) {
+    private val onDownloadCompleted = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            Toast.makeText(context, "Download complete", Toast.LENGTH_SHORT).show()
+        }
+    }
 
+    override fun downloadPhoto(photoUrl: String) {
+        context.registerReceiver(
+            onDownloadCompleted,
+            IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE)
+        )
         val request = DownloadManager.Request(Uri.parse(photoUrl))
             .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
             .setTitle(context.getString(R.string.download_title))
